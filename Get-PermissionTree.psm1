5c56fd4420d1528cda5086b8097fbf528eeb62e4
Eduard <daneduardleuska@gmail.com>
2025-10-16 15:19:40 +0200

    feat: add Get-PermissionTree function with optional depth and required path parameters that print out

--- Get-PermissionTree.psm1
+++ Get-PermissionTree.psm1
@@ -1,1 +1,18 @@
-# WIP
+# WIP!
+
+# Start of Get-PermissionTree.psm1
+function Get-PermissionTree {
+    param(
+        # Depth for how deep the tree goes (Standard: 1)
+        [int]$Depth = 1,
+        # Path to start from (Mandatory)
+        [Parameter(Mandatory=$true)][string]$Path = ""
+    )
+    # Outputs
+    Write-Host "Depth: $Depth"
+    Write-Host "Path: $Path"
+}
+
+# Export the function to make it available when the module is imported
+Export-ModuleMember -Function Get-PermissionTree
+# End of Get-PermissionTree.psm1
